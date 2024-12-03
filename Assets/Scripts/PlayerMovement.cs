3b5641dcb29d6a3f45a23fa272db1631ab57aa22
BleuApple <93416479+BleuApple@users.noreply.github.com>
2024-12-03 17:45:51 +0530

    added movement add echolocation from motion

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float speed;
-    [SerializeField] float gravityValue;
+    [SerializeField] float speed = 1;
+    [SerializeField] float gravityValue = 9.8f;
     [SerializeField] float GroundCheckRadius;
     [SerializeField] CharacterController controller;
     [SerializeField] Transform GroundCheck;
     [SerializeField] LayerMask groundLayer;
+    [SerializeField] float soundDuration = 2;
+    [SerializeField] float size = 3;
+    [SerializeField] GameObject EcholocationPrefab;
 
     private bool isGrounded;    
+    private float lastSoundStart;
     void Start()
     {
         controller  = GetComponent<CharacterController>();
+        lastSoundStart = -soundDuration - 1;
+
     }
 
     // Update is called once per frame
@@ -36,6 +44,32 @@ public class PlayerMovement : MonoBehaviour
            controller.Move(-Vector3.up * gravityValue * Time.deltaTime);
         }
 
-        
+        if (move != Vector3.zero){
+            if (Time.realtimeSinceStartup > lastSoundStart + soundDuration){
+                lastSoundStart = Time.realtimeSinceStartup;
+                SpawnSoundWave();
+            }
+        }
+
+
     }
+
+    void SpawnSoundWave(){
+        GameObject echoLocator = Instantiate(EcholocationPrefab, gameObject.transform.position, Quaternion.identity) as GameObject;
+        ParticleSystem echoWave = EcholocationPrefab.transform.GetChild(0).GetComponent<ParticleSystem>();
+
+        if (echoWave != null){
+            var main = echoWave.main;
+            main.startLifetime = soundDuration;
+            main.startSize = size;
+        }else{
+            Debug.Log("Empty/Erroneous echolocator\n");
+        }
+
+
+        Destroy(echoLocator, soundDuration + 1);
+    }
+
 }
+
+
