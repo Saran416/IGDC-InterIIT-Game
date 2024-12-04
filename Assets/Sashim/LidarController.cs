23ab0e08a683b601eb93df6b72031470437cd614
BleuApple <93416479+BleuApple@users.noreply.github.com>
2024-12-04 22:37:19 +0530

    trying new lidar

--- Assets/Sashim/LidarController.cs
+++ Assets/Sashim/LidarController.cs
@@ -1,35 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.VFX;
 
 public class LidarController : MonoBehaviour
 {
     public int numberOfPoints = 100;
-    public VisualEffect lidar_vfx;
+    public List<VisualEffect> lidar_vfxes;
     public Vector4[] lidarPoints;
     public Vector3[] lidarDirections;
 
-    bool canShoot = true;
+    public List<bool> canShootList;
     GraphicsBuffer positionBuffer;
     GraphicsBuffer directionBuffer;
+
+    public float lidarDuration = 60;
+    private float lidarDelay;
+    private float lastLidarShotTime;
+
+    bool canShoot;
     void Start()
     {
         lidarPoints = new Vector4[numberOfPoints*numberOfPoints];
         lidarDirections = new Vector3[numberOfPoints*numberOfPoints];
         positionBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, numberOfPoints*numberOfPoints, sizeof(float) * 4);
         directionBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, numberOfPoints*numberOfPoints, sizeof(float) * 3);
+        foreach (var lidar_vfx in lidar_vfxes){
+            canShootList.Add(true);
+        }
+
+        lidarDelay = (lidarDuration + 1)/lidar_vfxes.Count;
+        lastLidarShotTime = - lidarDelay - 1;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && canShoot){   
+        canShoot = false;
+        foreach (var shootability in canShootList){
+            if (shootability){
+                canShoot = true;
+                break;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && canShoot && Time.realtimeSinceStartup > lastLidarShotTime + lidarDelay){
+            lastLidarShotTime = Time.realtimeSinceStartup;   
             StartCoroutine(GetLidarPoints());
         }
     }
 
     IEnumerator GetLidarPoints(){
-        canShoot = false;
+        //int freeIndex = 0;
+        int freeIndex = 0;
+        for (int i = 0; i < canShootList.Count; i++){
+            freeIndex = i;
+            if (canShootList[i]) break;
+        }
+
+        canShootList[freeIndex] = false;
+        VisualEffect lidar_vfx = lidar_vfxes[freeIndex];
         lidar_vfx.Reinit();
         int pointcount = 0;
         for (float i=1; i < numberOfPoints; i++){
@@ -48,13 +78,13 @@ public class LidarController : MonoBehaviour
             lidar_vfx.SetGraphicsBuffer("LidarDirectionBuffer", directionBuffer);
             yield return null;
         }
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(lidarDuration);
         for (float i=1; i < numberOfPoints; i++){
             for (float j=1; j < numberOfPoints; j++){
                 lidarPoints[(int)(i*numberOfPoints + j)]  = Vector4.zero;
             }
         }
-        canShoot = true;
+        canShootList[freeIndex] = true;
     }
 
     void OnDestroy()
