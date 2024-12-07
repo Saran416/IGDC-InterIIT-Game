9cfb7334eaa9477a0a2cccb1e780bd11dc238297
BleuApple <93416479+BleuApple@users.noreply.github.com>
2024-12-07 16:49:18 +0530

    removed some scripts from prefab, toggled mesh renderer for grabbables, fixed tunnel 2

--- Assets/Scripts/grabbables.cs
+++ Assets/Scripts/grabbables.cs
@@ -8,26 +8,29 @@ public class grabbables : MonoBehaviour
     private Rigidbody grabTransfrom;
     private MeshCollider meshCollider;
     private Transform grab_point;
+    private MeshRenderer meshRenderer;
     
     private void Awake()
     {
         grab_point = null;
         grabTransfrom = GetComponent<Rigidbody>();
         meshCollider = GetComponent<MeshCollider>();
+        meshRenderer = GetComponent<MeshRenderer>();
     }
     public void Grab(Transform grab_point_transform){
         this.grab_point = grab_point_transform;
         grabTransfrom.isKinematic = true;
         grabTransfrom.useGravity = false;
         meshCollider.enabled = false;
-        
+        meshRenderer.enabled = false;
 
     }
     public void Drop(){
         this.grab_point = null;
         grabTransfrom.useGravity = true;
         meshCollider.enabled = true;
-        
+        meshRenderer.enabled = true;
+
         grabTransfrom.isKinematic=false;
         meshCollider.convex = true;
         {
