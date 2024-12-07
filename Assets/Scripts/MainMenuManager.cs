2ef950d06cdef29b1a4844e7d21efc53c1ccd372
Sashim Suryawanshi <sashimsuryawanshi@gmail.com>
2024-12-07 12:35:49 +0530

    MainMenu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject Main;
+    public GameObject Manual;
+    public GameObject[] Manual_pages;
+    
+    int page_counter = 0;
+    public void PlayPressed(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitPressed(){
+        Application.Quit();
+    }
+
+    public void ManualPressed(){
+        Main.SetActive(false);
+        Manual.SetActive(true);
+    }
+
+    public void NextPressed(){
+        Manual_pages[page_counter].SetActive(false);
+        page_counter++;
+        if (page_counter < Manual_pages.Length){
+            Manual_pages[page_counter].SetActive(true);
+        } else {
+            Manual_pages[0].SetActive(true);
+            Manual.SetActive(false);
+            Main.SetActive(true);
+            page_counter = 0;
+        }
+    }
+}
